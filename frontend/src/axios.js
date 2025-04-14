8aff11b89467c2f157e539d36b0091a171959301
srijan <srijanswapnil246@gmail.com>
2025-04-14 21:57:42 +0530

    h

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+//   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  baseURL:  "https://talk-a-tive-79vq.onrender.com",
+
   headers: {
     "Content-Type": "application/json",
   },
