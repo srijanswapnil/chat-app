18c479a51edfc30c417763ce97f18bd3e93bca56
srijan <srijanswapnil246@gmail.com>
2025-04-14 22:05:13 +0530

    seperated back and front end

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import userRoutes from './routes/userRoutes.js'
 import chatRoutes from './routes/chatRoutes.js'
 import cors from "cors"
 import messageRoutes from './routes/messageRoutes.js'
-import path from 'path'
+
 const app = express();
 app.use(cors());
 dotenv.config()
@@ -20,23 +20,6 @@ app.use('/api/chat',chatRoutes)
 app.use('/api/user',userRoutes)
 app.use('/api/message',messageRoutes)
 // .............   DEPLOYMENT  ................       //
-
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  console.log("jello bhai")
-
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
 
 //      ......................................       //
 
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-//   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
-  baseURL:  "https://talk-a-tive-79vq.onrender.com",
+
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
 
   headers: {
     "Content-Type": "application/json",
