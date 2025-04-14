1a3bd301976e585f47d3cba497379f4c45e88dee
srijan <srijanswapnil246@gmail.com>
2025-04-14 22:35:41 +0530

    j

--- frontend/src/Pages/Chatpage.jsx
+++ frontend/src/Pages/Chatpage.jsx
@@ -5,23 +5,21 @@ import MyChat from "../components/MyChat";
 import ChatBox from "../components/ChatBox";
 
 const Chatpage = () => {
-  const [h, setH] = useState(0);
-
   useEffect(() => {
-    if (h == 0) {
+    // Check sessionStorage for a reload flag
+    if (!sessionStorage.getItem("reloaded")) {
+      sessionStorage.setItem("reloaded", "true");
       window.location.reload();
-      setH((h) => h + 1);
     }
   }, []);
+
   const { user } = ChatState();
   const [fetchAgain, setFetchAgain] = useState(false);
+
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
-      <div
-        style={{ height: "91.5vh" }}
-        className="flex justify-between w-full p-10"
-      >
+      <div style={{ height: "91.5vh" }} className="flex justify-between w-full p-10">
         {user && <MyChat fetchAgain={fetchAgain} />}
         {user && (
           <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
