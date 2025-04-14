f4770a2973460cecef11f9d69b0430c4343de744
srijan <srijanswapnil246@gmail.com>
2025-04-14 22:29:22 +0530

    bak

--- frontend/src/Pages/Chatpage.jsx
+++ frontend/src/Pages/Chatpage.jsx
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
-import { ChatState } from '../Context/ChatProvider';
-import SideDrawer from '../components/miscellaneous/SideDrawer';
-import MyChat from '../components/MyChat';
-import ChatBox from '../components/ChatBox';
+import React, { useEffect, useState } from "react";
+import { ChatState } from "../Context/ChatProvider";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import MyChat from "../components/MyChat";
+import ChatBox from "../components/ChatBox";
 
 const Chatpage = () => {
-  const {user}=ChatState()
-  const [fetchAgain,setFetchAgain]=useState(false)
+  useEffect(() => {
+    window.location.reload();
+  });
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
   return (
-    <div style={{width:"100%"}}>
-      {user && <SideDrawer/>} 
-      <div style={{ height: "91.5vh" }} className='flex justify-between w-full p-10'>
-      {user && <MyChat fetchAgain={fetchAgain}  />} 
-      {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />}
-
-
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <div
+        style={{ height: "91.5vh" }}
+        className="flex justify-between w-full p-10"
+      >
+        {user && <MyChat fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </div>
-
     </div>
   );
 };
